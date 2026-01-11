bee71a070b1f485cdc056cd68d4ac1c3e7a6afb6
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2026-01-11 07:35:48 -0300

    feat: register CmsPaths and StubGenerator in CMSServiceProvider for improved configuration management

--- app-modules/cms/src/Providers/CMSServiceProvider.php
+++ app-modules/cms/src/Providers/CMSServiceProvider.php
@@ -5,13 +5,26 @@ declare(strict_types=1);
 namespace ClintonRocha\CMS\Providers;
 
 use ClintonRocha\CMS\CmsPanelPlugin;
+use ClintonRocha\CMS\Console\Helpers\CmsPaths;
+use ClintonRocha\CMS\Console\Helpers\StubGenerator;
 use Filament\Panel;
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 
 class CMSServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $this->mergeConfigFrom(__DIR__.'/../../../../config/cms.php', 'cms');
+
+        $this->app->singleton(CmsPaths::class, function ($app) {
+            return new CmsPaths($app->make(Filesystem::class));
+        });
+
+        $this->app->singleton(StubGenerator::class, function ($app) {
+            return new StubGenerator($app->make(Filesystem::class), $app->make(CmsPaths::class));
+        });
+
         Panel::configureUsing(static function (Panel $panel): void {
             match ($panel->getId()) {
                 'cms' => $panel->plugin(new CmsPanelPlugin()),
@@ -24,5 +37,12 @@ class CMSServiceProvider extends ServiceProvider
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'cms');
 
+        $this->publishes([
+            __DIR__.'/../../../../config/cms.php' => config_path('cms.php'),
+        ], 'cms-config');
+
+        $this->publishes([
+            __DIR__.'/../../stubs' => resource_path('stubs/cms'),
+        ], 'cms-stubs');
     }
 }
